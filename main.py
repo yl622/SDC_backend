b14101b5de7d5105ed2e3247b62f65aba017055c
Huang-Yu-Liang <hyl.cs11@nycu.edu.tw>
2024-09-29 22:32:37 +0800

    HW2: add /items/{item_id}

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from enum import Enum
+from typing import Union, Annotated
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class item(BaseModel):
+    name: str 
+    description: Union[str, None] = None
+    price: float 
+    tax: Union[float, None] = None
+# 由上至下檢查
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, Item:item, q: Union[str, None] = None):
+    result = {"item_id": item_id, **Item.model_dump()}
+    if q:
+        result.update({"q": q})
+    return result
